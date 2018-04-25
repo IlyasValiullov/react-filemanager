47f5872b0d1d3def9962ece844e3889609b1fb3b
Ilyas Valiullov <ilyas.valiullov@gmail.com>
2018-04-25 16:28:15 +0300

    check dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
-module.exports = env => {
-    const isProduction = env === 'production'
-    return {
-        entry: './app/assets/frontend/main.jsx',
-        devtool: isProduction ? 'source-map' : 'inline-source-map',
+const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports =
+    {
+        entry: path.join(__dirname, "src/lib"),
         output: {
-            path: __dirname + 'dist',
+            path: path.join(__dirname, 'dist'),
             filename: 'index.js'
         },
         resolve: {
-            extensions: ['', '.js', '.jsx']
+            extensions: ['.js', '.jsx']
         },
         module: {
             loaders: [
                 {
-                    test: /\.jsx?$/,
-                    loader: 'babel',
+                    test: /\.(js|jsx)$/,
+                    loader: 'babel-loader',
                     exclude: /node_modules/,
                     query: {
                         cacheDirectory: true,
                         presets: ['react', 'es2015']
                     }
+                },
+                {
+                    test: /\.css$/
+                    use: ["style-loader", "css-loader"]
                 }
             ]
-        },
-        externals: {
-            react: {
-                root: 'React',
-                commonjs2: 'react',
-                commonjs: 'react',
-                amd: 'react'
-            }
         }
-    }
-}
+        // },
+        // externals: {
+        //     react: {
+        //         root: 'React',
+        //         commonjs2:
+        //             'react',
+        //         commonjs:
+        //             'react',
+        //         amd:
+        //             'react'
+        //     }
+        // }
+    };
+// = env => {
+//     const isProduction = env === 'production'
+//     return {
