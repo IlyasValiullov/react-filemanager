aa19c8215c1fd4149c96fe608c033962ef421d4b
Ilyas Valiullov <ilyas.valiullov@gmail.com>
2018-04-26 12:51:39 +0300

    add demo app
    debug url

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React                from 'react'
+import FileManager        from 'components/filemanager'
+class App extends React.Component {
+    render() {
+        return (
+            <FileManager auth_url="http://localhost:3000/api/v1/admin/aws_auth" onSelect={this.fileSelected}>
+                <button name="myFile" >Hello</button>
+            </FileManager>
+        )
+    }
+}
+export default App;
