16ffaf45f455683c55fb07ce95484d1b2ff96400
Ilyas Valiullov <ilyas.valiullov@gmail.com>
2018-04-26 13:44:50 +0300

    fix demo app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React                from 'react'
 import FileManager        from 'components/filemanager'
 class App extends React.Component {
+
+    fileSelected =() => {
+        console.log("fileSelected");
+    }
+
     render() {
         return (
             <FileManager auth_url="http://localhost:3000/api/v1/admin/aws_auth" onSelect={this.fileSelected}>
